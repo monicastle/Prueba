4cee79e80e506063a1c8924fa45324d68de540ab
monicastle <monicastillo@unitec.edu>
2020-07-30 21:05:53 -0600

    Inicio del laboratorio

---
+++ Lab De Onasis/src/lab/de/onasis/LabDeOnasis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.de.onasis;
+
+/**
+ *
+ * @author Monica
+ */
+public class LabDeOnasis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        // Hola Onasis
+        int num;
+        //
+    }
+    
+}
